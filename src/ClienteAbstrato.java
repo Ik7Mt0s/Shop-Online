19b494ccce794a3969a9fe26952ff54637cece5b
Rhuan Douglas <rhuandmartises@gmail.com>
2025-11-19 23:39:39 -0300

    Create abstract Cliente class with basic attributes

---
+++ src/ClienteAbstrato.java
@@ -0,0 +1,27 @@
+public abstract class Cliente {
+    private int id;
+    private String nome;
+    private String email;
+    private double totalGasto;
+    protected double cashbackAcumulado;
+
+    public Cliente(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.totalGasto = 0.0;
+        this.cashbackAcumulado = 0.0;
+    }
+
+    public int getId() { return id; }
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public double getTotalGasto() { return totalGasto; }
+    public double getCashbackAcumulado() { return cashbackAcumulado; }
+
+    public void adicionarGasto(double valor) {
+        this.totalGasto += valor;
+    }
+
+    public abstract double calcularCashback(double valorCompra);
+}
